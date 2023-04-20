3d7bbd3d0d5274579707a086b24fcc0b12fe9278
Chawanakorn Akkayachinda <yecgaa2344@gmail.com>
2023-04-20 16:11:08 +0700

    add BaseWeapon field

--- src/weapon/BaseWeapon.java
+++ src/weapon/BaseWeapon.java
@@ -1,5 +1,24 @@
 package weapon;
 
 public abstract class BaseWeapon {
+	private String name;
+	private int damage;
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 }
